0f9ef6b59ab1afc31e3bfcffc706a0472cc87316
Neha Agarwal <nehavm1617@gmail.com>
2022-11-27 22:51:33 -0500

    Update Grid.java

--- Grid.java
+++ Grid.java
@@ -65,40 +65,30 @@ public class Grid {
     }
     // The best way IMO to calculate the number of groups is to set up a matrix of integers and
     // for each non-0 entry calculate the group it's in.
-    public ArrayList<Set<Spot>> calcAllGroups() {
-        // walking through every square in the N by N grid.
+    public ArrayList<Set<Spot>> calcAllGroups() {d.
         for(int i = 0; i < grid.length; i++){
             for(int j = 0; j < grid.length; j++){
-                // Creating set of spot  objects for every square we go through...
                 Set<Spot> set = new HashSet<>();
-                // Calling the private recursion method to find the group members...
                  recursion(set, i, j);
-                 // if the set is not empty, we add it to the allgroups array.
                  if(!set.isEmpty()){
                     allGroups.add(set);
                 }
             }
         }
-        // return the array.
         return allGroups;
 
     }
     public void recursion(Set<Spot> set, int i, int j){
-        // if i or j is not in the grid then return i.e. [0, N].
         if (j < 0 ||j > grid.length-1 || i < 0 || i > this.grid.length-1){
             return;
         }
-        // if spot (square) is not true (i.e. 0) then return because it is not in the group.
         if(!this.grid[i][j]){
             return;
         }
-        // Otherwise, set the square(i,j) in the grid to false.
         grid[i][j] = false;
-        // Create a spot object by passing i or j.
         Spot spot = new Spot(i, j);
         // add the spot into the allgroups array
         set.add(spot);
-        //recurse to the square's corresponding neighbour to search if it's in the group or not.
         recursion(set, i+1, j);
         recursion(set, i-1, j);
         recursion(set, i, j+1);
@@ -124,23 +114,18 @@ public class Grid {
      * @return the size of the group
      */
     public int groupSize(int i, int j) {
-        // i and j are not in the grid itself i.e. they cross the boundaries [0, N].
         if (j < 0 ||j > grid.length-1 || i < 0 || i > this.grid.length-1){
             throw new IndexOutOfBoundsException("Illegal i or j");
         }
-        // initialising the variables(keeps track of the number of members of the group) to 0
         int right = 0;
         int left = 0;
         int bottom = 0;
         int top = 0;
 
-        // If the square(i,j) in thr group is set as false, then return because it is not in the group.
         if(!this.grid[i][j]){
             return 0;
         }
-        // Set the square in the grid to false.
         grid[i][j] = false;
-        // Recurse to the corresponding neighbours of the square to search if they are a member of the group.
         right = groupSize(i,j+1);
         bottom = groupSize(i+1,j);
         left = groupSize(i,j-1);
